497ebe5a700f00fb2d8083120b5ff10d16ea9059
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-28 21:57:36 -0700

    Perfect

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-
 // socket.broadcast.emit('event', data)
 // socket.on('event', data)
 var express = require('express');
 var socket = require('socket.io');
-var io = socket(server);
-io.sockets.on('connection', newConnection)
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
 app.use(express.static('public'));
-
+var io = socket(server);
+io.sockets.on('connection', newConnection);
 
 var users = {};
 
 function newConnection(socket){
   console.log("Connected: " + socket.id);
-  socket.on('create_lobby', checkLobby);
-  function checkLobby(id){
+  socket.on('create_lobby', id=>{
     if(users[id]){
       console.log("lobby already exists");
       socket.emit('check_lobby_response', false);
@@ -24,5 +21,6 @@ function newConnection(socket){
       console.log("Lobby " + id + " created.");
       socket.emit('check_lobby_response', true);
     }
-  }
+  });
 }
+
